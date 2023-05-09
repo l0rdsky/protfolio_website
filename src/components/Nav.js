ed14a6bc631e2f3517ecd140af3d734fb1c35c3f
Akash pawar <ap28198@gmail.com>
2023-05-10 00:37:30 +0530

    removed unsued icons

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BiBriefcase, BiClipboard, BiHomeAlt, BiUser } from "react-icons/bi";
+import {  BiHomeAlt, BiUser } from "react-icons/bi";
 import { BsClipboardData, BsBriefcase, BsChatSquareText } from "react-icons/bs";
 import { Link } from "react-scroll";
 const Nav = () => {
