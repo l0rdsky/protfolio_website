8af0901baf5ec55c614dc2625372a6d4c9a79911
Akash pawar <ap28198@gmail.com>
2023-05-11 01:51:16 +0530

    added Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,20 @@
 import React from 'react';
-
+import Logo from '../assets/logo.svg'
 const Header = () => {
-  return <div className='section'>Header</div>;
+  return(
+    <header className='py-8'>
+       <div className='container mx-auto flex items-center justify-between '>
+           <a href='#' className='font-tertiary relative text-4xl header_logo'>
+           Akash</a>
+           <br/>
+           <a href='#' className='font-tertiary absolute text-2xl top-16 mx-10'>
+           Pawar
+           </a>
+           <button className='btn btn-sm'>Work with me</button>
+       </div>
+    </header>
+  ) 
+ 
 };
 
 export default Header;
